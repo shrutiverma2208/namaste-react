38da9e1b8fe5f6bd3296dbf2e21077b069f74500
shrutiverma2208 <shrutiverma2208@gmail.com>
2024-07-01 20:44:03 +0530

    adding header and body component to app

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
-// <div id="parent">
-// <div id="child">
-// <h1>Im h1 tag<h1>
-// </div>
-// </div>
+
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        'div',
-        {id:"child"},
-       [React.createElement("h1",{},"I'm h1 tag"),
-       React.createElement("h2",{},"I'm h2 jhghjgjo tag"),
-
-    ] 
-
+const Header =()=>{
+    return(
+    <div className="header">
+        <div className="logo-container">
+            <img className="logo" src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2'/>
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+    </div>
     )
-)
-console.log(parent)
+}
+const RestaurantCard =()=>{
+    return(
+        <div className="res-card">
+            <img  style={{width:"200px"}} 
+            alt="res-logo"
+            src="https://b.zmtcdn.com/data/pictures/chains/1/50691/ecfdc75d21bd7cd9d880f05c8382bc65.jpg"/>
+            <h3>Meghna Foods</h3>
+            <h4>Biryani,North Indian</h4>
+            <h5>4.5 stars</h5>
+            <h5>35 min</h5>
+        </div>
+    )
+}
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const Body=()=>{
+    return(
+        <div className="body-container">
+        <div className="search">
+          search
+        </div>
+        <div className="res-container">
+            <RestaurantCard/>
 
-root.render(parent);
+        </div>
+        </div>
+    )
+}
+
+
+const AppLayout=()=>{
+    return(
+    <div className="app">
+        <Header/>
+        <Body/>
+    </div>
+    )
+}
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppLayout/>)
